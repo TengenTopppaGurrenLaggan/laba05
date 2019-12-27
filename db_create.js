66e2c3c02dcd3661da727e9a3dfd617903880236
TengenTopppaGurrenLaggan <59069576+TengenTopppaGurrenLaggan@users.noreply.github.com>
2019-12-27 13:08:50 +0200

    ad

--- db_create.js
+++ db_create.js
@@ -6,7 +6,7 @@ const DB = new sqlite3.Database(DB_PATH) //коннект к файлу
 DB.serialize(() => {
     //запрос на создание если не существует
     DB.run(
-        'CREATE TABLE IF NOT EXISTS Kids (id INTEGER PRIMARY KEY UNIQUE, thing TEXT, price TEXT, customer TEXT)'
+        'CREATE TABLE IF NOT EXISTS Kids (id INTEGER PRIMARY KEY UNIQUE, thing TEXT, price TEXT, customer TEXT,age TEXT)'
     )
 })
 module.exports = DB

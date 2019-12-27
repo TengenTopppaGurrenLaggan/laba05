eaeea34e2121915fb41e6b35c339896fddd00f6f
TengenTopppaGurrenLaggan <59069576+TengenTopppaGurrenLaggan@users.noreply.github.com>
2019-12-27 12:30:14 +0200

    ad

--- route.js
+++ route.js
@@ -5,14 +5,6 @@ router.get('/', function(req, res) {
     //выбор пути
     res.sendFile(__dirname + '/public/index.html') //выбор файла
 })
-router.get('/public/gemini', function(req, res) {
-    res.sendFile(__dirname + '/public/gemini.html')
-})
-router.get('/public/oven', function(req, res) {
-    res.sendFile(__dirname + '/public/oven.html')
-})
-router.get('/public/kaban', function(req, res) {
-    res.sendFile(__dirname + '/public/kaban.html')
-})
+
 
 module.exports = router //возможность использование всех методов router в други js файлах

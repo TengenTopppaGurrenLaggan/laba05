9905eb49dbb237304d41dfe12374b32cae3ba5a8
TengenTopppaGurrenLaggan <59069576+TengenTopppaGurrenLaggan@users.noreply.github.com>
2019-12-27 13:04:14 +0200

    ad

--- db_create.js
+++ db_create.js
@@ -1,12 +1,12 @@
-const sqlite4 = require('sqlite4').verbose() //модуль базы данных
+const sqlite3 = require('sqlite3').verbose() //модуль базы данных
 const DB_PATH = 'mydatabase.db' //имя базы данных(путь)
-const DB = new sqlite4.Database(DB_PATH) //коннект к файлу
+const DB = new sqlite3.Database(DB_PATH) //коннект к файлу
 
 //задание параметров
 DB.serialize(() => {
     //запрос на создание если не существует
     DB.run(
-        'CREATE TABLE IF NOT EXISTS Kids (id INTEGER PRIMARY KEY UNIQUE, thing TEXT, price TEXT, customer TEXT,age TEXT)'
+        'CREATE TABLE IF NOT EXISTS Kids (id INTEGER PRIMARY KEY UNIQUE, thing TEXT, price TEXT, customer TEXT)'
     )
 })
 module.exports = DB
